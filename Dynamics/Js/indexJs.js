c4fba76ca908aa250e7fbdec4d155213414d92a8
Santiago-Alv <alvarado.santiago.school@comunidad.unam.mx>
2025-06-23 19:25:25 -0600

    feat[indexJs.js] añado JavaScript y realizo sistema de busqueda dentro de la base de datos buscador

---
+++ Dynamics/Js/indexJs.js
@@ -0,0 +1,24 @@
+let busquedaInput = document.getElementById("busqueda"); 
+
+
+busquedaInput.addEventListener("input",()=>{ 
+    let listaDeBusqueda  = []; //SE GUARDA LA LISTA DE BUSQUEDAS
+    let j = 0;
+    for(i=0;i<baseDatosJSON.artistas.length;i++){ //RECORRE EL ARREGLO ARTISTAS
+        let palabra = busquedaInput.value.toUpperCase(); //VUELVE LO QUE INGRESO EL USUARIO EN MAYUSCULAS
+        let artista = baseDatosJSON.artistas[i].nombre; 
+        artista = artista.toUpperCase(); //VUELVE AL NOMBRE DEL ARTISTA A MAYUS
+
+        if(artista.includes(palabra)){ //SI LA PALABRA QUE INGRESO EL USUARIO ESTA INCLUIDA
+          listaDeBusqueda[j] =  baseDatosJSON.artistas[i].nombre; //SE LA ASIGNA A LISTADEBUSQUEDA
+          j++; //RECORRE EL ARREGLO DE BUSQUEDA
+        }
+    }
+    listaDeBusqueda.sort(); //SORTEA POR ORDEN ALFABETICO LA LISTADEBUSQUEDA
+    listaDeBusqueda.sort((a,b)=>{ //SORTEA POR LO QUE INGRESO EL USUARIO "SI INGRESA N" => COLOCA LAS N PRIMERO EN EL ARREGLO
+        let posA = a.indexOf(busquedaInput.value); //BUSCA LA POSICION DE X EN A (SI ES N Y LA PALABRA ES NARANJA ES IGUAL A 0)
+        let posB = b.indexOf(busquedaInput.value); //BUSCA LA POSICION DE X EN B (SI ES N Y LA PALABRA ES MANZANA ES IGUAL A 2)
+        
+        return posA - posB; //SI EL VALOR A ES MENOR QUE EL VALOR B => A VA ANTES QUE B (0 - 2 = -2 como es negativo A va antes que B)
+    });
+});
