c1bc3005b7f94106e68892424102df4c9ba58c45
Santiago-Alv <alvarado.santiago.school@comunidad.unam.mx>
2025-06-25 17:13:50 -0600

    feat[indexJs.js] Funcionamiento de busqueda y vista de las diferentes secciones

--- Dynamics/Js/indexJs.js
+++ Dynamics/Js/indexJs.js
@@ -4,14 +4,40 @@ let busquedaInput = document.getElementById("busqueda");
 busquedaInput.addEventListener("input",()=>{ 
     let listaDeBusqueda  = []; //SE GUARDA LA LISTA DE BUSQUEDAS
     let j = 0;
+    const listaBusquedaHtml = document.getElementById("listaBusqueda");
+    if(busquedaInput.value.trim() === ""){
+        listaBusquedaHtml.innerHTML = '';
+        listaBusquedaHtml.style.display = "none"
+        return;
+    }
     for(i=0;i<baseDatosJSON.artistas.length;i++){ //RECORRE EL ARREGLO ARTISTAS
         let palabra = busquedaInput.value.toUpperCase(); //VUELVE LO QUE INGRESO EL USUARIO EN MAYUSCULAS
         let artista = baseDatosJSON.artistas[i].nombre; 
         artista = artista.toUpperCase(); //VUELVE AL NOMBRE DEL ARTISTA A MAYUS
 
         if(artista.includes(palabra)){ //SI LA PALABRA QUE INGRESO EL USUARIO ESTA INCLUIDA
-          listaDeBusqueda[j] =  baseDatosJSON.artistas[i].nombre; //SE LA ASIGNA A LISTADEBUSQUEDA
+          listaDeBusqueda[j] =  baseDatosJSON.artistas[i].nombre.trim(); //SE LA ASIGNA A LISTADEBUSQUEDA
           j++; //RECORRE EL ARREGLO DE BUSQUEDA
+        }
+    }
+    for(i=0;i<baseDatosJSON.album.length;i++){ //RECORRE EL ARREGLO ARTISTAS
+        let palabra = busquedaInput.value.toUpperCase(); //VUELVE LO QUE INGRESO EL USUARIO EN MAYUSCULAS
+        let album = baseDatosJSON.album[i].nombre; 
+        album = album.toUpperCase(); //VUELVE AL NOMBRE DEL ARTISTA A MAYUS
+
+        if(album.includes(palabra)){ //SI LA PALABRA QUE INGRESO EL USUARIO ESTA INCLUIDA
+            listaDeBusqueda[j] =  baseDatosJSON.album[i].nombre.trim(); //SE LA ASIGNA A LISTADEBUSQUEDA
+            j++; //RECORRE EL ARREGLO DE BUSQUEDA
+        }
+    }
+    for(i=0;i<baseDatosJSON.canciones.length;i++){ //RECORRE EL ARREGLO ARTISTAS
+        let palabra = busquedaInput.value.toUpperCase(); //VUELVE LO QUE INGRESO EL USUARIO EN MAYUSCULAS
+        let cancion = baseDatosJSON.canciones[i].nombre; 
+        cancion = cancion.toUpperCase(); //VUELVE AL NOMBRE DEL ARTISTA A MAYUS
+
+        if(cancion.includes(palabra)){ //SI LA PALABRA QUE INGRESO EL USUARIO ESTA INCLUIDA
+            listaDeBusqueda[j] =  baseDatosJSON.canciones[i].nombre.trim(); //SE LA ASIGNA A LISTADEBUSQUEDA
+            j++; //RECORRE EL ARREGLO DE BUSQUEDA
         }
     }
     listaDeBusqueda.sort(); //SORTEA POR ORDEN ALFABETICO LA LISTADEBUSQUEDA
@@ -21,31 +47,51 @@ busquedaInput.addEventListener("input",()=>{
         
         return posA - posB; //SI EL VALOR A ES MENOR QUE EL VALOR B => A VA ANTES QUE B (0 - 2 = -2 como es negativo A va antes que B)
     });
+    html ='';
+    for(i=0;i<listaDeBusqueda.length;i++){
+        html += `<div id="opcion" onclick="busqueda("${listaDeBusqueda[i]}")"><h1>${listaDeBusqueda[i]}</h1></div>`
+    }
+    listaBusquedaHtml.style.display = "block";
+    listaBusquedaHtml.innerHTML = html;
 });
+
+
+
+
+
+
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const homeBtn = document.getElementById("homeBtn");
-
 const artistsBtn = document.getElementById("artistsBtn");
 const playlistsBtn = document.getElementById("playlistsBtn");
 const creditsBtn = document.getElementById("creditsBtn");
+
+const sectionHome = document.getElementById("home");
+const sectionArtists = document.getElementById("artists");
+const sectionPlaylists = document.getElementById("playlist");
+const sectionCredits = document.getElementById("credits");
 
 activeColor = "#FDC787"; //DEFINE EL COLOR SI ESTA ACTIVO
 normalColor = "#ffff"; //DEFINE EL COLOR SI NO ESTA ACTIVO
 
 function btnActivo(btn){/* Establece el color del boton*/
-    homeBtn.style.color = (btn === homeBtn) ? activeColor : normalColor; 
+    homeBtn.style.color = (btn === homeBtn) ? activeColor : normalColor;
+    sectionHome.style.display = (btn === homeBtn) ? "block" : "none"; 
     artistsBtn.style.color = (btn === artistsBtn) ? activeColor : normalColor; 
+    sectionArtists.style.display = (btn === artistsBtn) ? "flex" : "none"; 
     playlistsBtn.style.color = (btn === playlistsBtn) ? activeColor : normalColor; 
+    sectionPlaylists.style.display = (btn === playlistsBtn) ? "flex" : "none"; 
     creditsBtn.style.color = (btn === creditsBtn) ? activeColor : normalColor;
+    sectionCredits.style.display = (btn === creditsBtn) ? "flex" : "none"; 
     /*Si el boton es igual al boton home por ejemplo, si eso devuelve TRUE el color se establece ACTIVECOLOR, si devuelve FALSE el color
     se establece  NORMALCOLOR*/
 }
 
 btnActivo(homeBtn); //ESTABLECE HOME CON EL COLOR DE ACTIVO AL INICIO DE LA PAGINA  
 
-homeBtn.addEventListener("click",()=> btnActivo(homeBtn)); //SI HACES CLICK LLAMA A LA FUNCION btnActivo(); y manda como parametro el btn presionado
+homeBtn.addEventListener("click",()=>{ btnActivo(homeBtn)}); //SI HACES CLICK LLAMA A LA FUNCION btnActivo(); y manda como parametro el btn presionado
 
 artistsBtn.addEventListener("click",()=> btnActivo(artistsBtn));
 
@@ -63,6 +109,17 @@ for(i=0;i<baseDatosJSON.artistas.length;i++){
 }
 
 artistas.innerHTML += html;
+
+html = '';
+html += `<h1>Artistas</h1>`
+for(i=0;i<baseDatosJSON.artistas.length;i++){
+    html+= `<div class="artista" onclick ="reproduce()" id="${baseDatosJSON.artistas[i].id}"><img src="${baseDatosJSON.artistas[i].url_img}"></div>`
+}
+html += `<h1>Albumes</h1>`
+for(i=0;i<baseDatosJSON.album.length;i++){
+    html+= `<div class="artista" onclick ="reproduce()" id="${baseDatosJSON.album[i].id}"><img src="${baseDatosJSON.album[i].url_img}"></div>`
+}
+sectionArtists.innerHTML += html;
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 function reproduce(){
     let reproductor = document.getElementById("reproductor")
